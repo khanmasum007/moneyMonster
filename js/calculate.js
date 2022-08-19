7e93c59d57011005e26a93dc2efecdaa3f546cf2
Masum <007khanmasum@gmail.com>
2022-08-19 23:51:20 +0600

    recap all problem

--- js/calculate.js
+++ js/calculate.js
@@ -20,15 +20,18 @@ const clothCost = getValueById ('cloth-cost')
 const income= getValueById('income');
 const newTotalExpenses= foodCost + rentCost + clothCost;
     const balance= income-newTotalExpenses;
+    if(newTotalExpenses < income){
+        document.getElementById('total-expenses').innerText= newTotalExpenses;
+   document.getElementById('balance').innerText= balance;
 
-   document.getElementById('total-expenses').innerText= newTotalExpenses;
-    document.getElementById('balance').innerText= balance;
+    }
+    else{
+        alert('kaku income er theke besi khoroch kora jabe na');
+    }
 
 })
 
 document.getElementById('saving-btn').addEventListener('click', function(){
-
-
     const foodCost= getValueById('food-cost');
 const rentCost =  getValueById ('rent-cost');
 const clothCost = getValueById ('cloth-cost')
@@ -38,9 +41,14 @@ const newTotalExpenses= foodCost + rentCost + clothCost;
 
 
     const savingParcent= getValueById('saving-parcent');
-    const savingTotal= (balance*savingParcent)/100;
-    document.getElementById('saving-total').innerText= savingTotal;
-
+    const savingTotal= (income*savingParcent)/100;
     const remaingBalance=  balance-savingTotal;
+
+   if(savingTotal<income){
+    document.getElementById('saving-total').innerText= savingTotal; 
     document.getElementById('remaing-balance').innerText= remaingBalance;
+   }
+   else{
+    alert('kaku ja ache tar besi saving hobe na')
+   }
 })
